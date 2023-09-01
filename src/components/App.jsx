99cac3bb154a37e92435d3f784fdf7fee58fca72
boblmbq <sichkardanylo@gmail.com>
2023-09-01 21:33:42 +0200

    first task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,3 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import userData from 'data/user.json';
+import { Profile } from './Profile/Profile';
+export const App = () => <Profile data={userData} />;
